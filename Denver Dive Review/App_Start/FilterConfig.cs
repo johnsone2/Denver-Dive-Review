5ec019853de0f0c9131f13704624ac6aa21b4387
Jon Bullock <jon@talentriot.com>
2013-08-01 19:24:50 -0600

    Initial MVC commit.

---
+++ Denver Dive Review/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Denver_Dive_Review
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
