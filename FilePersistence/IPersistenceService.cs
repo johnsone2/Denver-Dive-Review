5893a489a514c8b346fa87a6b3951ae95993db7f
Jon Bullock <jon@talentriot.com>
2014-02-13 20:43:25 -0700

    Added file persistence service, bar tab file service.

---
+++ FilePersistence/IPersistenceService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FilePersistence
+{
+    interface IPersistenceService<T>
+    {
+        IEnumerable<T> Read();
+        int Create(T barTab);
+        T Read(int id);
+        bool Update(T barTab);
+        bool Destroy(int id);
+    }
+}
