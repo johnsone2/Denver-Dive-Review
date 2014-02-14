588e76af10cafdf8cf9ada67123ba2ed1a7df938
Jon Bullock <jon@talentriot.com>
2014-02-13 20:53:14 -0700

    Add bartab api controller.

---
+++ DenverDiveApi/Controllers/BarTabController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Http;
+using BarTabService;
+using Domain;
+using Newtonsoft.Json;
+
+namespace DenverDiveApi.Controllers
+{
+    public class BarTabController : ApiController
+    {
+        // GET api/bartab
+        public IEnumerable<BarTab> Get()
+        {
+            var barTabService = new BarTabFileService();
+            return barTabService.Read();
+        }
+
+        // POST api/bartab
+        public void Post([FromBody]BarTab barTab)
+        {
+            var barTabService = new BarTabFileService();
+            barTabService.Create(barTab);
+        }
+    }
+}
