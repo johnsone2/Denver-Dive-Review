defc0faf6e997905f971aa3957bf4c3572884ebb
Jon Bullock <jon@talentriot.com>
2014-03-18 19:07:28 -0600

    Added connection specs for local db connectivity.

---
+++ DbPersistenceSpecs/DbConnectivitySpecs.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Npgsql;
+using NUnit.Framework;
+
+namespace DbPersistenceSpecs
+{
+    [TestFixture]
+    public class DbConnectivitySpecs
+    {
+        private static readonly string ConnectionString =
+            ConfigurationManager.ConnectionStrings["diveDb"].ConnectionString;
+
+        [Test]
+        public void We_should_be_able_to_connect_to_the_db()
+        {
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "select 1;";
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            
+            // passes if no exception thrown.
+        }
+    }
+}
